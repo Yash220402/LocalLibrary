dfbd151aa345be5158b8949be24048c2da85974b
Yash220402 <yash220402@gmail.com>
2022-08-08 20:44:11 +0530

    update admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Author, Genre, Book, BookInstance, Language
 
-# Register your models here.
+
+# admin.site.register(Book)
+@admin.register(Book)
+
+class BookAdmin(admin.ModelAdmin):
+    def display_genre(self):
+        """Create a string for the genre. This is required to diplay genre in admin."""
+        return ", ".join(genre.name for genre in self.genre.all()[:3])
+    display_genre.short_description = 'Genre'
+
+    list_display = ("title", "author", display_genre)
+
+
+# admin.site.register(Author)
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("last_name", "first_name", "date_of_birth", "date_of_death")
+# Register the admin class with the associated model
+# admin.site.register(Author, AuthorAdmin)
+
+# admin.site.register(BookInstance)
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Genre)
+admin.site.register(Language)
