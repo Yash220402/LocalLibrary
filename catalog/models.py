c0beb3fd961dc07510fd742cab5aa762683facf6
Yash220402 <yash220402@gmail.com>
2022-08-08 19:17:13 +0530

    create Language and Author models

--- catalog/models.py
+++ catalog/models.py
@@ -12,6 +12,14 @@ class Genre(models.Model):
         """String for representing the model object."""
         return self.name
 
+class Language(models.Model):
+    """Model representing a Language"""
+    name = models.CharField(max_length=200,
+    help_text="Enter book's natural language")
+
+    def __str__(self):
+        """String for representing the model object."""
+        return self.name
 
 class Book(models.Model):
     """Model representing a book (but not specific copy of a book)."""
@@ -29,6 +37,8 @@ class Book(models.Model):
 
     # Genre class has already had already been defined so we can specify the object above
     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+
+    language = models.ForeignKey("Language", on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         """String for representing the Model object"""
@@ -70,3 +80,20 @@ class BookInstance(models.Model):
     def __str__(self):
         """String for representing the Model object"""
         return f"{self.id} ({self.book.title})"
+
+class Author(models.Model):
+    """Model representing an author."""
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField("Died", null=True, blank=True)
+
+    class Meta:
+        ordering = ["last_name", "first_name"]
+
+    def get_absolute_url(self):
+        """Returns the URL to access a particular author instance."""
+        return reverse('author-details', args=[str(self.id)])
+
+    def __str__(self):
+        return f"{self.last_name}, {self.first_name}"
