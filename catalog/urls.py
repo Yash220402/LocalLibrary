a595c5604ec381062f21ccec4aa35c508ebb4b9a
Yash220402 <yash220402@gmail.com>
2022-08-09 20:53:12 +0530

    add book_list book_details templates

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,11 @@
+from xml.etree.ElementInclude import include
 from django.urls import path
 from.import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
 
+    # we use <int:pk> to capture the book id, which must be a specifically formatted string as pass it to the view as a parameter named pk (primary key).
+    path('books/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,23 @@
-from multiprocessing import context
+from turtle import title
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+    # context_object_name = 'book_list'
+    # queryset = Book.objects.filter(title__icontains='war')[:5]
+    # template_name = 'books/my_arbitrary_template_name_list.html'
+    def get_context_data(self, **kwargs):
+        # call the base implementation first to get the context
+        context = super(BookListView, self).get_context_data(**kwargs)
+
+        # create any data and add it to context
+        context['some_data'] = "This is just some data"
+        return context
+
+class BookDetailView(generic.DetailView):
+    model = Book
 
 # Create your views here.
 def index(request):
@@ -16,11 +33,15 @@ def index(request):
     # The 'all()' is implied by default
     num_authors = Author.objects.count()
 
+    # genres count
+    num_genres = Genre.objects.count()
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_genres': num_genres,
     }
 
     # render the HTML template index.html with the data in the context variable
