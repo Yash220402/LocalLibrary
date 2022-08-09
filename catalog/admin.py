c54d73cb0eaebae07b22b26108ad84a72e89e5cc
Yash220402 <yash220402@gmail.com>
2022-08-09 14:40:11 +0530

    update admin model

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,8 +3,10 @@ from .models import Author, Genre, Book, BookInstance, Language
 
 
 # admin.site.register(Book)
-@admin.register(Book)
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
 
+@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     def display_genre(self):
         """Create a string for the genre. This is required to diplay genre in admin."""
@@ -12,19 +14,30 @@ class BookAdmin(admin.ModelAdmin):
     display_genre.short_description = 'Genre'
 
     list_display = ("title", "author", display_genre)
+    inlines = [BookInstanceInline]
 
 
 # admin.site.register(Author)
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ("last_name", "first_name", "date_of_birth", "date_of_death")
+    fields = ["first_name", "last_name",  ("date_of_birth", "date_of_death")]
 # Register the admin class with the associated model
 # admin.site.register(Author, AuthorAdmin)
 
 # admin.site.register(BookInstance)
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ("status", "due_back")
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
 
 admin.site.register(Genre)
 admin.site.register(Language)
