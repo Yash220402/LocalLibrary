96e83f0d43c1ec93351d7ac56847cdc73f0fbae3
Yash220402 <yash220402@gmail.com>
2022-08-16 02:51:29 +0530

    add book list template

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,11 @@
-from xml.etree.ElementInclude import include
-from django.urls import path
+from django.urls import include, re_path, path
 from.import views
+import re
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
+    path("book/<int:pk>", views.BookDetailView.as_view(), name="book-detail")
 
-    # we use <int:pk> to capture the book id, which must be a specifically formatted string as pass it to the view as a parameter named pk (primary key).
-    path('books/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+    # path("book/<int:pk>", views.BookDetailView.as_view(), name="book-detail")
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -3,19 +3,6 @@ from .models import Book, Author, BookInstance, Genre
 from django.views import generic
 from django.shortcuts import get_object_or_404
 
-class BookListView(generic.ListView):
-    model = Book
-    # context_object_name = 'book_list'
-    # queryset = Book.objects.filter(title__icontains='war')[:5]
-    # template_name = 'books/my_arbitrary_template_name_list.html'
-    def get_context_data(self, **kwargs):
-        # call the base implementation first to get the context
-        context = super(BookListView, self).get_context_data(**kwargs)
-
-        # create any data and add it to context
-        context['some_data'] = "This is just some data"
-        return context
-
 # Create your views here.
 def index(request):
     """View function for home page of site"""
@@ -29,7 +16,6 @@ def index(request):
 
     # The 'all()' is implied by default
     num_authors = Author.objects.count()
-
     # genres count
     num_genres = Genre.objects.count()
 
@@ -44,8 +30,12 @@ def index(request):
     # render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
 
-class BookDetailView(generic.DetailView):
+class BookListView(generic.ListView):
     model = Book
+
+class BookDetailView(generic.DetailView):
+    model = Book 
+
     def book_detail_view(request, primary_key):
         book = get_object_or_404(Book, pk=primary_key)
-        return render(request, 'catalog/book_detail.html', context={'book': book})
+        return render(request, "catalog/book_detail.html", context={'book': book})
