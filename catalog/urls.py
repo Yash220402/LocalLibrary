242b4cf28c0fbb5ccb01e79214443f133111d3a6
Yash220402 <yash220402@gmail.com>
2022-08-16 03:21:56 +0530

    removed book-details template errors

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,11 +1,8 @@
-from django.urls import include, re_path, path
-from.import views
-import re
+from django.urls import path
+from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
-    path("book/<int:pk>", views.BookDetailView.as_view(), name="book-detail")
-
-    # path("book/<int:pk>", views.BookDetailView.as_view(), name="book-detail")
+    path("book/<int:pk>", views.BookDetailView.as_view(), name="book-detail"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -34,8 +34,4 @@ class BookListView(generic.ListView):
     model = Book
 
 class BookDetailView(generic.DetailView):
-    model = Book 
-
-    def book_detail_view(request, primary_key):
-        book = get_object_or_404(Book, pk=primary_key)
-        return render(request, "catalog/book_detail.html", context={'book': book})
+    model = Book
