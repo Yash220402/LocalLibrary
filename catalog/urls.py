3771d33969a1f40eac3544b57eec8f7e145fba6d
Yash220402 <yash220402@gmail.com>
2022-08-20 09:52:04 +0530

    Added loaned book by user list view

--- catalog/urls.py
+++ catalog/urls.py
@@ -13,3 +13,7 @@ urlpatterns = [
 urlpatterns += [
     path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+urlpatterns += [
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+]
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,8 @@
+from asyncio import mixins
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 def index(request):
@@ -47,3 +49,12 @@ class AuthorListView(generic.ListView):
 
 class AuthorDetailView(generic.DetailView):
     model = Author
+
+class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
+    """Generic class-view listing loaned book to current user"""
+    model = BookInstance
+    template_name = "catalog/bookinstance_list_borrowed_user.html"
+    paginated_by = 2
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
